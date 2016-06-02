986c6b405fd19b22f0476f9bbde6e013e3b8d497
senorlewis <derrickrlewis@gmail.com>
2016-06-02 17:10:59 -0500

    Initial commit

---
+++ src/main/java/com/theironyard/TrackPackDemo10Application.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackPackDemo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackPackDemo10Application.class, args);
+	}
+}
